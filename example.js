bad409b35d47c8c065a7997c8770584552e93674
KingOfSolo <944125893@qq.com>
2017-01-11 13:19:13 +0800

    test2.2

--- example.js
+++ example.js
@@ -1,5 +1,7 @@
-var mood="happy",age=33,height="about 5'10\" tall";
-var beatles=["John",1940,false];
-alert(mood);
-alert(age);
-alert(height);
+var mood = "happy",age = 33,height = "about 5'10\" tall";
+var lennon = {name:"John",year:1940,living:false};
+var beatles = {};
+beatles.vocalist=lennon;
+alert(beatles.vocalist.name);
+alert(beatles.vocalist.year);
+alert(beatles.vocalist.living);
