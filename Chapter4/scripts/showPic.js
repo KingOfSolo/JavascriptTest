d0d09f35e099126280e8b1bf8a32ba45859f7674
KingOfSolo <944125893@qq.com>
2017-01-16 22:41:38 +0800

    test4.3

--- Chapter4/scripts/showPic.js
+++ Chapter4/scripts/showPic.js
@@ -3,4 +3,14 @@ function showPic(whichPic) {
 	var source = whichPic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src",source);
+	var text = whichPic.getAttribute("title");
+	var description = document.getElementById("description");
+	description.firstChild.nodeValue = text;
 }
+
+function countBodyChildren() {
+	var body_element = document.getElementByTagName("body")[0];
+	alert(body_element.childNodes.length);
+}
+
+window.onload = countBodyChildren();
