35594c4043c12eb290d8c0eb77ff59a9921aca07
KingOfSolo <944125893@qq.com>
2017-01-21 12:29:15 +0800

    test7.2

--- Chapter4/scripts/showPic.js
+++ Chapter4/scripts/showPic.js
@@ -1,33 +1,4 @@
 
-function showPic(whichPic) {
-	if(!document.getElementById("placeholder")) return false;
-	var source = whichPic.getAttribute("href");
-	var placeholder = document.getElementById("placeholder");
-	if (placeholder.nodeName != "IMG") return false;
-	placeholder.setAttribute("src",source);
-	if(document.getElementById("description")){
-		var text = whichPic.getAttribute("title") ? whichPic.getAttribute("title") : "";
-		var description = document.getElementById("description");
-		if (description.firstChild.nodeType == 3) {
-			description.firstChild.nodeValue = text;
-		}
-	}
-	return true;
-}
-
-function prepareGallery(){
-	if(!document.getElementById) return false;
-	if(!document.getElementByTagName) return false;
-	if(!document.getElementById("imagegallery")) return false;
-	var gallery = document.getElementById("imagegallery");
-	var links = gallery.getElementByTagName("a");
-	for(var i = 0;i < links.length;i++){
-		links[i].onclick = function(){
-			return showPic(this) ? false : true;
-		}
-	}
-}
-
 function addLoadEvent(func){
 	var oldonload = window.onload;
 	if(typeof window.onload != 'function'){
@@ -40,10 +11,69 @@ function addLoadEvent(func){
 	}
 }
 
+function insertAfter(newElement,targetElement){
+	var parent = targetElement.parentNode;
+	if(parent.lastChild == targetElement){
+		parent.appendChild(newElement);
+	} else {
+		parent.insertBefore(newElement,targetElement.nextSibling);
+	}
+}
+
+function preparaPlaceholder(){
+	if(!document.createElement) return false;
+	if(!document.createTextNode) return false;
+	if(!document.getElementById) return false;
+	if(!document.getElementById("imagegallery")) return false;
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","images/placeholder.gif");
+	placeholder.setAttribute("alt","my image gallery");
+	var description = document.createElement("p");
+	description.setAttribute("id","description");
+	var desctext = document.createTextNode("Choose an image");
+	description.appendChild(desctext);
+	var gallery = document.getElementById("imagegallery");
+	insertAfter(placeholder,gallery);
+	insertAfter(description,placeholder);
+}
+
+function prepareGallery(){
+	if(!document.getElementById) return false;
+	if(!document.getElementByTagName) return false;
+	if(!document.getElementById("imagegallery")) return false;
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementByTagName("a");
+	for(var i = 0;i < links.length;i++){
+		links[i].onclick = function(){
+			return showPic(this);
+		}
+		links[i].onkeypress = links[i].onclick;
+	}
+}
+
+function showPic(whichPic) {
+	if(!document.getElementById("placeholder")) return true;
+	var source = whichPic.getAttribute("href");
+	var placeholder = document.getElementById("placeholder");
+	placeholder.setAttribute("src",source);
+	if(!document.getElementById("description")) return false;
+	if(whichPic.getAttribute("title")){
+		var text = whichPic.getAttribute("title");
+	} else {
+		var text = "";
+	}
+	var description = document.getElementById("description");
+	if(description.firstChild.nodeType == 3){
+		description.firstChild.nodeValue = text;
+	}
+	return false;
+}
 // function countBodyChildren() {
 // 	var body_element = document.getElementByTagName("body")[0];
 // 	alert(body_element.childNodes.length);
 // }
 
+addLoadEvent(preparaPlaceholder);
 addLoadEvent(prepareGallery);
 
--- example.js
+++ example.js
@@ -4,10 +4,16 @@ var beatles = {};
 beatles.vocalist=lennon;
 
 window.onload = function() {
-	var testdiv = document.getElementById("testdiv");
 	var para = document.createElement("p");
+	var txt1 = document.createTextNode("This is");
+	para.appendChild(txt1);
+	var emphasis = document.createElement("em");
+	var txt2 = document.createTextNode(" my");
+	emphasis.appendChild(txt2);
+	para.appendChild(emphasis);
+	var txt3 = document.createTextNode(" content.");
+	para.appendChild(txt3);
+	var testdiv = document.getElementById("testdiv");
 	testdiv.appendChild(para);
-	var txt = document.createTextNode("Hello world");
-	para.appendChild(txt);
 }
 
